4c3e7a8db2702129295a2eca322d839cbb9c3668
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-11-15 18:55:06 +0800

    update file

---
+++ counter_cubit/lib/presentation/screens/home_screen.dart
@@ -0,0 +1,102 @@
+import 'package:counter_cubit/logic/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+//BloC Listener practice
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented == true) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        "Incremented!",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      duration: Duration(milliseconds: 5),
+                      backgroundColor: Colors.amber,
+                    ),
+                  );
+                } else if (state.wasIncremented == false) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        "Decrement!",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      duration: Duration(milliseconds: 5),
+                      backgroundColor: Colors.green,
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                return Text(
+                  state.counterValue.toString(),
+                  style: const TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    // BlocProvider.of<CounterCubit>(context).decrement();
+                    context.read<CounterCubit>().decrement();
+                  },
+                  tooltip: "Decrement",
+                  child: const Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  tooltip: "Add",
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
