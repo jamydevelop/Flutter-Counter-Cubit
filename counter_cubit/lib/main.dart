1eb2f1d4425add680d7839838e2870ed5c939906
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-11-15 18:54:46 +0800

    update directory

--- counter_cubit/lib/main.dart
+++ counter_cubit/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:counter_cubit/cubit/counter_cubit.dart';
+import 'package:counter_cubit/logic/cubit/counter_cubit.dart';
+import 'package:counter_cubit/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -22,106 +23,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade200),
           useMaterial3: true,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-//BloC Listener practice
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-                if (state.wasIncremented == true) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text(
-                        "Incremented!",
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                      duration: Duration(milliseconds: 5),
-                      backgroundColor: Colors.amber,
-                    ),
-                  );
-                } else if (state.wasIncremented == false) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text(
-                        "Decrement!",
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                      duration: Duration(milliseconds: 5),
-                      backgroundColor: Colors.green,
-                    ),
-                  );
-                }
-              },
-              builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                  style: const TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                  ),
-                );
-              },
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    // BlocProvider.of<CounterCubit>(context).decrement();
-                    context.read<CounterCubit>().decrement();
-                  },
-                  tooltip: "Decrement",
-                  child: const Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  tooltip: "Add",
-                  child: const Icon(Icons.add),
-                ),
-              ],
-            )
-          ],
-        ),
+        home: const HomeScreen(title: 'Flutter Demo Home Page'),
       ),
     );
   }
